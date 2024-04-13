a9813bf7657818bae547769a9cda0a1451389e78
isaac <isaac@DESKTOP-RLSB13Q>
2024-04-12 23:41:02 -0500

    agregar

--- lab2.m
+++ lab2.m
@@ -1,14 +1,14 @@
 clc
 close all
 clear all
-SNR=200;
-fc=80;
-Pn=20;
+SNR=20;
+fc=8000;
+Pn=5;
 [y,t,w,Tobs]=Senialyruido(SNR,fc,Pn);
 
-% figure
-% plot(f, abs(Yf)); title("Respuesta en magnitud de la salida");
-% xlabel("frecuencia"); ylabel("Magnitud");
+ figure
+ plot(f, abs(Yf)); title("Respuesta en magnitud de la salida");
+ xlabel("frecuencia"); ylabel("Magnitud");
 % 
 % figure
 % plot(f,angle(Yf)); title("Respuesta en fase de la salida");
