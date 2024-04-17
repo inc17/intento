b33a050fdc1b67a962a5f5d37c306af1ad414231
inc17 <159938875+inc17@users.noreply.github.com>
2024-04-16 21:29:13 -0500

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -1,15 +1,23 @@
 function plan = buildfile
 
-% Add the source folder to the path
+% Agregar la carpeta de origen al path
 addpath("simu");
 
-% Create a plan
-plan = buildplan(localfunctions);
+% Crear un plan
+plan = buildplan(@buildTasks);
 
-% Add a task to run tests
-plan("test") = matlab.buildtool.tasks.TestTask("tests");
+% Agregar una tarea para ejecutar las pruebas
+plan("test") = matlab.buildtool.tasks.TestTask(buildTasks);
 
-% Make the "test" task the default task in the plan
+% Establecer la tarea "test" como la tarea predeterminada en el plan
 plan.DefaultTasks = "test";
+
+end
+
+function tasks = buildTasks()
+% Esta función devuelve las tareas que se deben incluir en el plan de construcción
+
+% Agregar aquí todas las tareas que necesites
+tasks = ["test"]; % Puedes agregar más tareas aquí si es necesario
 
 end
